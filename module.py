f43f5e70b2fbec6e85e92dcc08fa7c27985a3815
swhitt1987 <126019018+swhitt1987@users.noreply.github.com>
2023-03-08 00:04:33 -0500

    Add files via upload

---
+++ module.py
@@ -0,0 +1,14 @@
+def area():
+     length = int(input("Please enter the length of the house: "))
+     width = int(input("Please enter the width of the house: "))
+     area = length * width
+     print(area)
+area()
+
+def circumference():
+     diameter = int(input("What is the diameter of the circle in inches?: "))
+     pi = 3.14
+     circumference = pi * diameter
+     print(circumference)
+circumference()
+
